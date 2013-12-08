e336e80084a104c934a503df373a133d481b63fe
slixurd <fatearcadia@gmail.com>
2013-12-08 13:21:10 +0800

    add:inode,block基本数据结构

--- disk/disk.h
+++ disk/disk.h
@@ -3,6 +3,120 @@
 
 #include<iostream>
 #include<fstream>
+#include<ctime>
+#include<bitset>
 using namespace std;
+
+struct SuperBlock{
+/*
+ * 超级块
+ * 每个块组前都有一个备份
+ * 
+ */
+    int blockSize;//block大小,单位byte
+    long unusedBlockCount;//未使用block数目
+    long totalBlockCount;//总计block数目
+    long unusedInodeCount;//未使用inode数目
+    long totalInodeCount;//总计inode数目
+    long blockGroupCount;//block组数目
+    int blockPerGroupNum;//每组block数目
+    int inodePerGroupNum;//每组inode数目
+};
+
+struct GDT{
+/*块组描述符
+ * Group Descriptor Table
+ *
+ */
+    long blockLoc;
+    long inodeLoc;
+    long inodeTableLoc;
+    int unusedInodeCount;
+    int unusedBlockCount;
+    int directoryCount;//目录数
+};
+
+struct inode{
+    int mod;//权限,采用位操作,1,2,4分别代表root,user,guest
+    unsigned iCount;//引用次数
+    long size;
+    bool isDirectory;
+    int uid;//拥有者id
+    //gid_t gid;//群组id,暂时不做分组
+    time_t mtime;//修改时间
+    time_t atime;//读取时间
+    time_t ctime;//创建时间
+    int blocks;//使用了的block个数
+    long block[6];
+    long redirect1;//间接指针,指向inode number
+    long redirect2;
+    long redirect3;
+};
+
+
+struct GroupHeader{
+    SuperBlock superBlock;
+    GDT xGDT;
+//头部后面的数据
+//    block bitmap;
+//    inode bitmap;
+//    inode table;
+//    data;
+};
+
+
+class FileSystem{
+public:
+    FileSystem(fstream* _fs){
+        fs = _fs;
+    
+    }
+
+    bool initDisk(int _blockSize){
+        if(!fs)
+            return false;
+        superBlock = new SuperBlock;
+        bs=_blockSize;
+        superBlock->blockSize = _blockSize;
+        
+        // get the size of the whole disk
+        fs->clear();
+        fs->seekg(0,ios::end);
+        diskSize = fs->tellg();
+        // count the number of block when the size and block size defined;
+        superBlock->totalBlockCount = diskSize/_blockSize;
+        superBlock->unusedBlockCount=superBlock->totalBlockCount;
+        // map仅占用一个blocksize
+
+        
+        
+    }
+
+//文件操作列表
+
+
+
+//存取步骤
+//1.确定是否具有权限
+//2.根据inode bitmap找到没有使用的inode号码,写入权限/属性
+//3.根据block bitmap找到没有使用的号码,将文件写入block,并将inode的block指向这个号码
+//4.更新两个bitmap,更新superBlock
+private:
+    int currentLocation;//当前在文件中的位置
+    int diskSize;
+    int bs;
+    int bitmapSize;
+
+    SuperBlock *superBlock;
+    fstream* fs;
+    
+    bool headerToDisk(){
+        if(fs==NULL)return false;
+        fs->seekg(0);
+        fs->seekp(0);
+        //fs->write();
+    
+    }
+};
 
 #endif
--- disk/main.cpp
+++ disk/main.cpp
@@ -1,27 +1,5 @@
 #include "disk.h"
 
-class FileSystem{
-    public:
-        FileSystem(){
-        
-        }
-
-        bool initDisk();
-//文件操作列表
-
-
-
-//存取步骤
-//1.确定是否具有权限
-//2.根据inode bitmap找到没有使用的inode号码,写入权限/属性
-//3.根据block bitmap找到没有使用的号码,将文件写入block,并将inode的block指向这个号码
-//4.更新两个bitmap,更新superblock
-    private:
-        int currentLocation;//当前在文件中的位置
-
-
-
-};
 
 //SuperBlock
 //空闲的bitmap表
@@ -31,8 +9,36 @@ class FileSystem{
 
 //资料存放区:inode table,data block
 //metadata:bitmap,superblock
+
+void bm(){
+    int t=1024;
+    bitset<4096> inodeMap;
+    inodeMap.reset();
+}
+
 int main(){
-
-
+    
+        bm();
+    int j=0x65171734;
+    bitset<512> inodeMap(j);
+    //fstream f("./a.dat",ios::out|ios::binary);
+    //f.write((char*)&inodeMap,2);
+    fstream f("./a.dat",ios::in|ios::binary);
+    int al;
+    f.read((char*)&al,2);
+    cout<<al;
+    
+    //SuperBlock s;
+    //s.blockSize=1024;
+    //s.unusedBlockCount=100;
+    //s.unusedInodeCount=100;
+    //s.totalBlockCount=100;
+    //s.blockGroupCount=35;
+    //SuperBlock t;
+    //cout<<f.tellg()<<" "<<f.tellp()<<endl;
+    //f.read((char*) &t,sizeof(SuperBlock));
+    //cout<<f.tellg()<<" "<<f.tellp()<<endl;
+    //cout<<t.unusedBlockCount<<" "<<t.blockSize;
+    //f.close();
 }
 
